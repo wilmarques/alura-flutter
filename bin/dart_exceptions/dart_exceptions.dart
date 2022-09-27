c7f756f63afd4d4789b59783f34eac77a6b84960
wilmarques <wileymarques@live.com>
2022-09-27 13:24:42 +0000

    WIP - Dart: lidando com Exceptions e Null Safety - Aula 3 - Criando exceções personalizadas

--- bin/dart_exceptions/dart_exceptions.dart
+++ bin/dart_exceptions/dart_exceptions.dart
@@ -1,39 +1,30 @@
 import 'controllers/bank_controller.dart';
+import 'exceptions/bank_controller_exceptions.dart';
 import 'models/account.dart';
 
-// void main() {
-//   // Criando o banco
-//   BankController bankController = BankController();
-
-//   // Adicionando contas
-//   bankController.addAccount(
-//       id: "Ricarth",
-//       account:
-//           Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
-
-//   bankController.addAccount(
-//       id: "Kako",
-//       account:
-//           Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
-
-//   // Fazendo transferência
-//   bool result = bankController.makeTransfer(
-//       idSender: "Kako", idReceiver: "Ricarth", amount: 700);
-
-//   // Observando resultado
-//   print(result);
-// }
-
 void main() {
-  try {
-    testFunc();
-  } on Exception catch (e) {
-    print('Exception');
-  } catch (e) {
-    print('Qualquer');
-  }
-}
+  // Criando o banco
+  BankController bankController = BankController();
 
-void testFunc() {
-  throw 'Teste';
+  // Adicionando contas
+  bankController.addAccount(
+      id: "Ricarth",
+      account:
+          Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
+
+  bankController.addAccount(
+      id: "Kako",
+      account:
+          Account(name: "Caio Couto", balance: 1000, isAuthenticated: true));
+
+  try {
+    // Fazendo transferência
+    bool result = bankController.makeTransfer(
+        idSender: "Kako222", idReceiver: "Ricarth", amount: 700);
+
+    // Observando resultado
+    print(result);
+  } on SenderIdInvalidException catch (e) {
+    print('O ID ${e.idSender} do remetente não é um ID válido');
+  }
 }

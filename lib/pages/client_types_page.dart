54ac536f3e6da81baf1b7bd7cf9106e96fc33110
Wil Marques <wileymarques@live.com>
2023-10-27 12:26:23 +0000

    WIP - Gerenciamento de estado

--- lib/pages/client_types_page.dart
+++ lib/pages/client_types_page.dart
@@ -13,7 +13,6 @@ class ClientTypesPage extends StatefulWidget {
 }
 
 class _ClientTypesPageState extends State<ClientTypesPage> {
-
   List<ClientType> types = [
     ClientType(name: 'Platinum', icon: Icons.credit_card),
     ClientType(name: 'Golden', icon: Icons.card_membership),
@@ -25,7 +24,6 @@ class _ClientTypesPageState extends State<ClientTypesPage> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -52,7 +50,9 @@ class _ClientTypesPageState extends State<ClientTypesPage> {
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.deepOrange,
-        onPressed: (){createType(context);},
+        onPressed: () {
+          createType(context);
+        },
         tooltip: 'Add Tipo',
         child: const Icon(Icons.add),
       ),
@@ -63,69 +63,71 @@ class _ClientTypesPageState extends State<ClientTypesPage> {
     TextEditingController nomeInput = TextEditingController();
 
     showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          scrollable: true,
-          title: const Text('Cadastrar tipo'),
-          content: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Form(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  TextFormField(
-                    controller: nomeInput,
-                    decoration: const InputDecoration(
-                      labelText: 'Nome',
-                      icon: Icon(Icons.account_box),
-                    ),
-                  ),
-                  const Padding(padding: EdgeInsets.all(5)),
-                  StatefulBuilder(builder: (BuildContext context, StateSetter setState) {
-                    return Column(children: [
-                      const Padding(padding: EdgeInsets.all(5)),
-                      selectedIcon != null ? Icon(selectedIcon, color: Colors.deepOrange) : const Text('Selecione um ícone'),
-                      const Padding(padding: EdgeInsets.all(5)),
-                      SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                            onPressed: () async {
-                              final IconData? result = await showIconPicker(context: context, defalutIcon: selectedIcon);
-                              setState(() {
-                                selectedIcon = result;
-                              });
-                            },
-                            child: const Text('Selecionar icone')
-                        ),
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            scrollable: true,
+            title: const Text('Cadastrar tipo'),
+            content: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Form(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    TextFormField(
+                      controller: nomeInput,
+                      decoration: const InputDecoration(
+                        labelText: 'Nome',
+                        icon: Icon(Icons.account_box),
                       ),
-                    ]);
-                  }),
-                ],
+                    ),
+                    const Padding(padding: EdgeInsets.all(5)),
+                    StatefulBuilder(
+                        builder: (BuildContext context, StateSetter setState) {
+                      return Column(children: [
+                        const Padding(padding: EdgeInsets.all(5)),
+                        selectedIcon != null
+                            ? Icon(selectedIcon, color: Colors.deepOrange)
+                            : const Text('Selecione um ícone'),
+                        const Padding(padding: EdgeInsets.all(5)),
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                              onPressed: () async {
+                                final IconData? result = await showIconPicker(
+                                    context: context,
+                                    defalutIcon: selectedIcon);
+                                setState(() {
+                                  selectedIcon = result;
+                                });
+                              },
+                              child: const Text('Selecionar icone')),
+                        ),
+                      ]);
+                    }),
+                  ],
+                ),
               ),
             ),
-          ),
-          actions: [
-            TextButton(
-              child: const Text("Salvar"),
-              onPressed: () {
-                selectedIcon ??= Icons.credit_score;
-                types.add(ClientType(name: nomeInput.text, icon: selectedIcon));
-                selectedIcon = null;
-                setState(() {});
-                Navigator.pop(context);
-              }
-            ),
-            TextButton(
-              child: const Text("Calcelar"),
-              onPressed: () async {
-                selectedIcon = null;
-                Navigator.pop(context);
-              }
-            )
-          ],
-        );
-      }
-    );
+            actions: [
+              TextButton(
+                  child: const Text("Salvar"),
+                  onPressed: () {
+                    selectedIcon ??= Icons.credit_score;
+                    types.add(
+                        ClientType(name: nomeInput.text, icon: selectedIcon));
+                    selectedIcon = null;
+                    setState(() {});
+                    Navigator.pop(context);
+                  }),
+              TextButton(
+                  child: const Text("Calcelar"),
+                  onPressed: () async {
+                    selectedIcon = null;
+                    Navigator.pop(context);
+                  })
+            ],
+          );
+        });
   }
 }
